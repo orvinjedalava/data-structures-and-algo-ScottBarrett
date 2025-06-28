5778663e6d2b6c111596bd78ad335767be6e19d0
orvinjedalava <ojedbub@gmail.com>
2025-06-28 22:24:54 +1000

    finsihed section 2 - 10

---
+++ Section-002-BigO/010-O(1)/script.js
@@ -0,0 +1,8 @@
+// The function below has a time complexity of O(1)
+// because it performs a constant number of operations regardless of the input size.
+
+function addItems(n) {
+  return n + n + n;
+}
+
+addItems(10);
