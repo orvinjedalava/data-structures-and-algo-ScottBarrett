76e0650740b57e1281c1307bacd2bd0d940b6d67
orvinjedalava <ojedbub@gmail.com>
2025-06-29 22:22:53 +1000

    finished section 4 - 21 CSharp

---
+++ Section-003-LinkedList/csharp/program.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Text.Json;
+
+var linkedList = new LinkedListObject(10);
+Console.WriteLine(JsonSerializer.Serialize(linkedList));
+
+public class LinkedListObject
+{
+  public LinkedListObject(int value)
+  {
+    var node = new Node(value);
+    this.Head = node;
+    this.Tail = this.Head;
+    this.Length = 1;
+  }
+
+  public Node Head { get; set; }
+  public Node Tail { get; set; }
+  public int Length { get; private set; }
+
+  public void PrintList()
+  {
+    Console.WriteLine(this);
+    Node currentNode = this.Head;
+    while (currentNode != null)
+    {
+      Console.WriteLine(currentNode.Value);
+      currentNode = currentNode.Next;
+    }
+  }
+
+}
+
+public class Node
+{
+  public Node(int value)
+  {
+    this.Value = value;
+    this.Next = null;
+  }
+
+  public int Value { get; private set; }
+  public Node Next { get; set; }
+}
+
