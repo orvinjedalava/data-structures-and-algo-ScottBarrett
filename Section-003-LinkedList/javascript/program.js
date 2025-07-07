c6c4116f91a7a9d0394c0b43aa004c9b7c3719f3
orvinjedalava <ojedbub@gmail.com>
2025-07-07 22:37:57 +1000

    finished javascript section 4 - 24

--- Section-003-LinkedList/javascript/program.js
+++ Section-003-LinkedList/javascript/program.js
@@ -1,5 +1,13 @@
 import LinkedList from './LinkedList.js';
 
-let myLinkedList = new LinkedList(7);
-myLinkedList.push(4)
-myLinkedList.printList();
+let myLinkedList_Pop = new LinkedList(7);
+myLinkedList_Pop.push(4)
+myLinkedList_Pop.printList();
+
+let popped = myLinkedList_Pop.pop();
+myLinkedList_Pop.printList();
+console.log(`Popped value: ${popped.value}`);
+
+popped = myLinkedList_Pop.pop();
+myLinkedList_Pop.printList();
+console.log(`Popped value: ${popped.value}`);
