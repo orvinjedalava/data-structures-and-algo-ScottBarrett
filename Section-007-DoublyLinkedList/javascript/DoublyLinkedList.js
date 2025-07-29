93f30640bfac4aad8b1452cb943e68b47e7769b2
orvinjedalava <ojedbub@gmail.com>
2025-07-29 22:47:31 +1000

    section 7 - 41

--- Section-007-DoublyLinkedList/javascript/DoublyLinkedList.js
+++ Section-007-DoublyLinkedList/javascript/DoublyLinkedList.js
@@ -10,11 +10,35 @@ export default class DoublyLinkedList {
 
   printList() {
     let currentNode = this.head;
+    console.log("length:", this.length);
     while (currentNode) {
       console.log(currentNode.value);
-      currentNode = currentNode.next;
+      currentNode = currentNode.next;``
+    }
+  }
+
+  printListBackwards() {
+    let currentNode = this.tail;
+    console.log("length:", this.length);
+    while (currentNode) {
+      console.log(currentNode.value);
+      currentNode = currentNode.prev;
     }
   }
 
   // Add methods for the doubly linked list here
+  push(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
--- Section-007-DoublyLinkedList/javascript/program.js
+++ Section-007-DoublyLinkedList/javascript/program.js
@@ -1,4 +1,11 @@
 import DoublyLinkedList from "./DoublyLinkedList.js";
 
+console.log("Doubly Linked List Example");
 const myDoublyLinkedList = new DoublyLinkedList(7);
 myDoublyLinkedList.printList();
+
+console.log("Adding nodes to the doubly linked list via push:");
+myDoublyLinkedList.push(10);
+myDoublyLinkedList.push(15);
+myDoublyLinkedList.printList();
+myDoublyLinkedList.printListBackwards();
