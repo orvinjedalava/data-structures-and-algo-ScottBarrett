c8571d31c46daec88dd7e161e3431ea3055cf98a
orvinjedalava <ojedbub@gmail.com>
2025-07-29 22:36:13 +1000

    Section 7 - 40

---
+++ Section-007-DoublyLinkedList/javascript/DoublyLinkedList.js
@@ -0,0 +1,20 @@
+import Node from './Node.js';
+
+export default class DoublyLinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  printList() {
+    let currentNode = this.head;
+    while (currentNode) {
+      console.log(currentNode.value);
+      currentNode = currentNode.next;
+    }
+  }
+
+  // Add methods for the doubly linked list here
+}
---
+++ Section-007-DoublyLinkedList/javascript/program.js
@@ -0,0 +1,4 @@
+import DoublyLinkedList from "./DoublyLinkedList.js";
+
+const myDoublyLinkedList = new DoublyLinkedList(7);
+myDoublyLinkedList.printList();
