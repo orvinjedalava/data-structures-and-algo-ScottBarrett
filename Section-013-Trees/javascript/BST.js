fac6c3eb9be4525526b2a383a3e92ede85c1cbaf
orvinjedalava <ojedbub@gmail.com>
2025-08-19 10:27:41 +1000

    Section #13 - 70

--- Section-013-Trees/javascript/BST.js
+++ Section-013-Trees/javascript/BST.js
@@ -44,5 +44,25 @@ export default class BST {
         temp = temp.right;
       }
     }
-  } 
+  }
+  
+  contains(value) {
+    if (!this.root) {
+      return false;
+    }
+
+    let temp = this.root;
+
+    while(temp) {
+      if (value < temp.value) {
+        temp = temp.left;
+      } else if (value > temp.value) {
+        temp = temp.right;
+      } else {
+        return true;
+      }
+    }
+
+    return false;
+  }
 }
--- Section-013-Trees/javascript/program.js
+++ Section-013-Trees/javascript/program.js
@@ -15,3 +15,7 @@ bst.printTree();
 console.log('Inserting value of 27...');
 bst.insert(27);
 bst.printTree();
+
+console.log('Is 21 in the tree? ' , bst.contains(21) ? 'yes' : 'no');
+console.log('Is 33 in the tree? ' , bst.contains(33) ? 'yes' : 'no');
+console.log('Is 52 in the tree? ' , bst.contains(52) ? 'yes' : 'no');
