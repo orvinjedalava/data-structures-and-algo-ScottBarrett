20601895a86cd069ffccdd8b9bce5a9a04f8f3ab
orvinjedalava <ojedbub@gmail.com>
2025-08-19 10:09:20 +1000

    Section #13 - 69

--- Section-013-Trees/javascript/BST.js
+++ Section-013-Trees/javascript/BST.js
@@ -4,4 +4,45 @@ export default class BST {
   constructor() {
     this.root = null;
   }
+
+  printTree() {
+    function inOrder(node) {
+      if (!node) return;
+      inOrder(node.left);
+      console.log(node.value);
+      inOrder(node.right);
+    }
+    inOrder(this.root);
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    let temp = this.root;
+
+    while (true) {
+      if (newNode.value == temp.value) {
+        return undefined; // No duplicates allowed
+      }
+
+      if (newNode.value < temp.value) {
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        }
+        temp = temp.left;
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode;
+          return this;
+        }
+        temp = temp.right;
+      }
+    }
+  } 
 }
--- Section-013-Trees/javascript/program.js
+++ Section-013-Trees/javascript/program.js
@@ -2,3 +2,16 @@ import BST from './BST.js';
 
 const bst = new BST();
 console.log(bst);
+
+console.log('Building the initial tree...');
+bst.insert(47);
+bst.insert(21);
+bst.insert(76);
+bst.insert(18);
+bst.insert(52);
+bst.insert(82);
+bst.printTree();
+
+console.log('Inserting value of 27...');
+bst.insert(27);
+bst.printTree();
