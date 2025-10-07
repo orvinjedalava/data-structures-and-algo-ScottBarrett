36277202d490e1e97629f4dd1c7a02c296c51369
orvinjedalava <ojedbub@gmail.com>
2025-10-08 10:07:26 +1100

    Section 15 - 75

--- Section-015-HashTables/javascript/program.js
+++ Section-015-HashTables/javascript/program.js
@@ -1,8 +1,16 @@
 import HashTable from './HashTable.js';
 
-let myHashTable = new HashTable();
-myHashTable.set('lumber', 70);
-myHashTable.set('bolts', 1400);
-myHashTable.set('washers', 50);
+let myHashTable1 = new HashTable();
+myHashTable1.set('lumber', 70);
+myHashTable1.set('bolts', 1400);
+myHashTable1.set('washers', 50);
 
-console.table(myHashTable.dataMap);
+console.table(myHashTable1.dataMap);
+
+let myHashTable2 = new HashTable();
+myHashTable2.set('bolts', 1400);
+myHashTable2.set('washers', 50);
+
+console.log(myHashTable2.get('bolts'));
+console.log(myHashTable2.get('washers'));
+console.log(myHashTable2.get('lumber')); // undefined
