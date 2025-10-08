452640872c76453c3219cb0800fa2a51e7d225a4
orvinjedalava <ojedbub@gmail.com>
2025-10-08 11:40:41 +1100

    Section 18 - 85

--- Section-018-Graph/javascript/Graph.js
+++ Section-018-Graph/javascript/Graph.js
@@ -10,4 +10,13 @@ export default class Graph {
     }
     return false;
   }
+
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1].push(vertex2);
+      this.adjacencyList[vertex2].push(vertex1);
+      return true;
+    }
+    return false;
+  }
 }
--- Section-018-Graph/javascript/program.js
+++ Section-018-Graph/javascript/program.js
@@ -6,3 +6,12 @@ myGraph1.addVertex('B');
 myGraph1.addVertex('C');
 
 console.log(myGraph1.adjacencyList);
+
+let myGraph2 = new Graph();
+myGraph2.addVertex('A');
+myGraph2.addVertex('B');
+myGraph2.addVertex('C');
+myGraph2.addEdge('A', 'B');
+myGraph2.addEdge('A', 'C');
+
+console.log(myGraph2.adjacencyList);
