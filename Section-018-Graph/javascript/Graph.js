a5755cf4fa24e2bd53f4e81815e3727bd0d684df
orvinjedalava <ojedbub@gmail.com>
2025-10-08 11:34:37 +1100

    Section #18 - 84

---
+++ Section-018-Graph/javascript/Graph.js
@@ -0,0 +1,13 @@
+export default class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+      return true;
+    }
+    return false;
+  }
+}
---
+++ Section-018-Graph/javascript/program.js
@@ -0,0 +1,8 @@
+import Graph from './Graph.js';
+
+let myGraph1 = new Graph();
+myGraph1.addVertex('A');
+myGraph1.addVertex('B');
+myGraph1.addVertex('C');
+
+console.log(myGraph1.adjacencyList);
