86c7bf8e9014438e17df74f3162eff366e37e6d3
orvinjedalava <ojedbub@gmail.com>
2025-10-12 10:54:55 +1100

    Section 20 - 91

--- Section-020-Heap/javascript/Heap.js
+++ Section-020-Heap/javascript/Heap.js
@@ -21,4 +21,21 @@ export default class Heap {
   #swap(index1, index2) {
     [this.#heap[index1], this.#heap[index2]] = [this.#heap[index2], this.#heap[index1]];
   }
+
+  print() {
+    console.log(this.#heap);
+  }
+
+  insert(value) {
+    this.#heap.push(value);
+    let current = this.#heap.length - 1;
+
+    while (
+      current > 0
+      && this.#heap[current] > this.#heap[this.#parent(current)]
+    ) {
+      this.#swap(current, this.#parent(current));
+      current = this.#parent(current);
+    }
+  }
 }
---
+++ Section-020-Heap/javascript/program.js
@@ -0,0 +1,14 @@
+import Heap from './Heap.js';
+
+const myHeap1 = new Heap();
+myHeap1.insert(99);
+myHeap1.insert(72);
+myHeap1.insert(61);
+myHeap1.insert(58);
+myHeap1.print();
+
+myHeap1.insert(100);
+myHeap1.print();
+
+myHeap1.insert(75);
+myHeap1.print();
