cea12f280dc1bd6af497e7aa4072f63dfb416cbe
orvinjedalava <ojedbub@gmail.com>
2025-10-12 14:00:42 +1100

    Section 22 - 99

--- Section-022-RecursiveBST/javascript/program.js
+++ Section-022-RecursiveBST/javascript/program.js
@@ -1,20 +1,27 @@
 import BST from './BST.js';
 
-const bst = new BST();
-console.log(bst);
+const bst1 = new BST();
+console.log(bst1);
 
 console.log('Building the initial tree...');
-bst.insert(47);
-bst.insert(21);
-bst.insert(76);
-bst.insert(18);
-bst.insert(27);
-bst.insert(52);
-bst.insert(82);
-bst.printTree();
+bst1.insert(47);
+bst1.insert(21);
+bst1.insert(76);
+bst1.insert(18);
+bst1.insert(27);
+bst1.insert(52);
+bst1.insert(82);
+bst1.printTree();
 
 console.log('\nBST Contains 27:');
-console.log(bst.contains(27) ? 'yes' : 'no');
+console.log(bst1.contains(27) ? 'yes' : 'no');
 
 console.log('\nBST Contains 17:');
-console.log(bst.contains(17) ? 'yes' : 'no');
+console.log(bst1.contains(17) ? 'yes' : 'no');
+
+const bst2 = new BST();
+bst2.rInsert(2);
+bst2.rInsert(1);
+bst2.rInsert(3);
+
+bst2.printTree();
