b90dc2fc2b9894f477e433cbe7c7b3872a31bc5b
orvinjedalava <ojedbub@gmail.com>
2025-10-15 16:01:57 +1100

    Section 25 - 117

--- Section-025-BasicSort/javascript/program.js
+++ Section-025-BasicSort/javascript/program.js
@@ -12,5 +12,27 @@ function bubbleSort(array) {
   return array;
 }
 
+function selectionSort(array) {
+  let min;
+  for (let i = 0; i < array.length - 1; i++) {
+    min = i;
+    for(let j = i+1; j < array.length; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+    if (i !== min) {
+      let temp = array[i];
+      array[i] = array[min];
+      array[min] = temp;
+    }
+    
+  }
+
+  return array;
+}
+
 console.log('Bubble Sort');
 console.log(bubbleSort([4, 2, 6, 5, 1, 3]));
+console.log('Selection Sort');
+console.log(selectionSort([4, 2, 6, 5, 1, 3]));
