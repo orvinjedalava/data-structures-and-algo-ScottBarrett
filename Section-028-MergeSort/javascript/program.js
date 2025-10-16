98c77376c020fb108c8f30e4cf7754bcf6f843c4
orvinjedalava <ojedbub@gmail.com>
2025-10-16 16:53:43 +1100

    Section 28 - 125

--- Section-028-MergeSort/javascript/program.js
+++ Section-028-MergeSort/javascript/program.js
@@ -24,5 +24,18 @@ function merge(array1, array2) {
   return combined;
 }
 
+function mergeSort(array) {
+  if (array.length === 1) return array;
+
+  let midIndex = Math.floor(array.length / 2);
+  let left = mergeSort(array.slice(0, midIndex));
+  let right = mergeSort(array.slice(midIndex));
+
+  return merge(left, right);
+}
+
 console.log('Merge 2 arrays');
 console.log(merge([1, 3, 7, 8], [2, 4, 5, 6]));
+
+console.log('Merge Sort');
+console.log(mergeSort([8, 3, 5, 4, 7, 6, 1, 2]));
