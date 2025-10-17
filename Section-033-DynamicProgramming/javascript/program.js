0ed7d38c832d4148bea79b1c8e285d203c76becb
orvinjedalava <ojedbub@gmail.com>
2025-10-17 11:11:24 +1100

    Section 33 - 136

--- Section-033-DynamicProgramming/javascript/program.js
+++ Section-033-DynamicProgramming/javascript/program.js
@@ -15,6 +15,22 @@ function fib(n) {
   return memo[n];
 }
 
+let counterBottomUp = 0;
+function fibBottomUp(n) {
+  let fibArray = [];
+  fibArray[0] = 0;
+  fibArray[1] = 1;
+
+  for (let index = 2; index <= n; index++) {
+    counterBottomUp++;
+    fibArray[index] = fibArray[index - 1] + fibArray[index - 2];
+  }
+
+  return fibArray[n];
+}
+
 const n = 7;
 console.log('\nFib of', n, '=', fib(n));
 console.log('\nCounter =', counter);
+console.log('\nFib of', n, '=', fibBottomUp(n));
+console.log('\nCounter Bottom Up =', counterBottomUp);
